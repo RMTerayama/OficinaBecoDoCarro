2cf1e264b717b192a17a5e99a7d7af0898120e48
Richard Masayuki Terayama <rmterayama.2000@gmail.com>
2022-04-21 16:54:46 -0400

    16.54_21.04.22

--- Oficina/src/App.java
+++ Oficina/src/App.java
@@ -1,5 +1,59 @@
+/*_
+   ___   __ _      _               ____                       _          _____                     
+ / __ \ / _(_)    (_)             |  _ \                     | |        / ____|                    
+| |  | | |_ _  ___ _ _ __   __ _  | |_) | ___  ___ ___     __| | ___   | |     __ _ _ __ _ __ ___  
+| |  | |  _| |/ __| | '_ \ / _` | |  _ < / _ \/ __/ _ \   / _` |/ _ \  | |    / _` | '__| '__/ _ \ 
+| |__| | | | | (__| | | | | (_| | | |_) |  __/ (_| (_) | | (_| | (_) | | |___| (_| | |  | | | (_) |
+ \____/|_| |_|\___|_|_| |_|\__,_| |____/ \___|\___\___/   \__,_|\___/   \_____\__,_|_|  |_|  \___/                                                                                                 
+                                                                                          
+*/
+import java.util.Scanner;
+
+import org.w3c.dom.Text;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int option=1;
+        Scanner input =new Scanner(System.in);
+        while(option!=0){
+            Text.BecoDoCarro();
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            System.out.println("\n1 - Gerenciar clientes\n2 - Gerenciar peças\n3 - Gerenciar serviços\n4 - Gerenciar ordens de serviço\n5 - Consultar total vendido em um período\n6 - Sair do programa\n");
+            System.out.print("Opção: ");
+            option=input.nextInt();
+            if(option==1){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                System.out.println("\n1 - Cadastrar\n2 - Consultar por CPF\n3 - Excluir\n4 - Editar\n5 - Listar todos os cadastros\n6 - Voltar");
+                System.out.print("Opção: ");
+                option=input.nextInt();
+
+            }
+            if(option==2){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                System.out.println("\n1 - Cadastrar\n2 - Consultar por código\n3 - Excluir\n4 - Editar\n5 - Listar todos os cadastros\n6 - Voltar");
+                System.out.print("Opção: ");
+                option=input.nextInt();
+            }
+            if(option==3){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                System.out.println("\n1 - Cadastrar\n2 - Consultar por código\n3 - Excluir\n4 - Editar\n5 - Listar todos os cadastros\n6 - Voltar");
+                System.out.print("Opção: ");
+                option=input.nextInt();
+            }
+            if(option==4){
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                System.out.println("\n1 - Abrir nova ordem de serviço\n2 - Gerenciar itens\n3 - Cancelar\n4 - Finalizar\n5 - Excluir\n6 - Listar todas as ordens\n7 - Voltar");
+                System.out.print("Opção: ");
+                option=input.nextInt();
+            }
+            /*if(option==5){
+
+            }*/
+
+        }
+
+
+
+
+
     }
 }
