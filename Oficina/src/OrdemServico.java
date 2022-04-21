5724eb53918b25798bc6bf8a3f513aee6b326f33
Richard Masayuki Terayama <rmterayama.2000@gmail.com>
2022-04-21 16:00:42 -0400

    Organizando

---
+++ Oficina/src/OrdemServico.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class OrdemServico {
+    int OSNumber;
+    Date OSdate;
+    String FinalDateOS;
+    String LicensePlate;
+    String Situation;
+    public class ItemOS{
+        String ItemType;
+        Double Price;
+        int Amount;
+    }
+}
