cdc016337f53d3aeb50bca5c01b64e56c357b243
Richard Masayuki Terayama <rmterayama.2000@gmail.com>
2022-04-28 21:35:28 -0400

    auterações no metodo de cadastrar cliente

--- Oficina/src/Classes/ClienteFunção.java
+++ Oficina/src/Classes/ClienteFunção.java
@@ -6,32 +6,29 @@ import java.util.Scanner;
 public class ClienteFunção {
     static Scanner input =new Scanner(System.in);
 
-    public static Cliente Cadastrar(){
-        Cliente cliente;
+    public static Cliente Cadastrar(Cliente cliente){
+        System.out.println("__CADASTRO CLIENTE__");
+        System.out.printf("\nInsira o Nome do cliente:");
+        cliente.nome            =input.next();
 
-        String nome;
-        String cpf;
-        long fone;
-        String rua;
-        int numero;
-        String bairro;
-        String cep;
-
-        nome   =input.next();
-        cpf    =input.next();
-        fone   =input.nextLong();
-        rua    =input.next();
-        numero =input.nextInt();
-        bairro =input.next();
-        cep    =input.next();
-        
-        cliente.nome            =nome;
+        System.out.println("\nInsira o CPF do cliente:");
         cliente.cpf             =input.next();
+
+        System.out.printf("\nInsira o TELEFONE do cliente:");
         cliente.fone            =input.nextLong();
+
+        System.out.printf("\nInsira a rua do cliente:");
         cliente.endereço.rua    =input.next();
+
+        System.out.printf("\nInsira o numero da casa do cliente:");
         cliente.endereço.numero =input.nextInt();
+
+        System.out.printf("\nInsira o bairro do cliente:");
         cliente.endereço.bairro =input.next();
-        cliente.endereço.cep    =cep;
+
+        System.out.printf("\nInsira o cep do cliente:");
+        cliente.endereço.cep    =input.next();
+
 
         return cliente;
     }
